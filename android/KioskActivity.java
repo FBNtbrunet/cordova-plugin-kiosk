e35d96215c872045c45b6a6c2dfb9a34069dbea1
FBNtbrunet <tbrunet@feedbacknow.com>
2019-10-03 17:42:16 +0200

    wip fix error overlay

--- android/KioskActivity.java
+++ android/KioskActivity.java
@@ -21,101 +21,102 @@ import java.util.TimerTask;
 
 public class KioskActivity extends CordovaActivity {
 
-    public static volatile boolean running = false;
-    public static volatile Set<Integer> allowedKeys = Collections.EMPTY_SET;
+  public static volatile boolean running = false;
+  public static volatile Set<Integer> allowedKeys = Collections.EMPTY_SET;
 
-    private StatusBarOverlay statusBarOverlay = null;
+  private StatusBarOverlay statusBarOverlay = null;
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        running = true;
+  @Override
+  protected void onStart() {
+    super.onStart();
+    System.out.println("KioskActivity started");
+    running = true;
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    System.out.println("KioskActivity stopped");
+    running = false;
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    super.init();
+
+    if (running) {
+      finish(); // prevent more instances of kiosk activity
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        running = false;
-    }
+    loadUrl(launchUrl);
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        super.init();
-        
-        if (running) {
-            finish(); // prevent more instances of kiosk activity
+    // https://github.com/apache/cordova-plugin-statusbar/blob/master/src/android/StatusBar.java
+    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+    getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+    // https://github.com/hkalina/cordova-plugin-kiosk/issues/14
+    View decorView = getWindow().getDecorView();
+    // Hide the status bar.
+    decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+    // Remember that you should never show the action bar if the
+    // status bar is hidden, so hide that too if necessary.
+    ActionBar actionBar = getActionBar();
+    if (actionBar != null) actionBar.hide();
+
+    // add overlay to prevent statusbar access by swiping
+    statusBarOverlay = StatusBarOverlay.createOrObtainPermission(this);
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    if (statusBarOverlay != null) {
+      statusBarOverlay.destroy(this);
+      statusBarOverlay = null;
+    }
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    ActivityManager activityManager = (ActivityManager) getApplicationContext()
+      .getSystemService(Context.ACTIVITY_SERVICE);
+    activityManager.moveTaskToFront(getTaskId(), 0);
+  }
+
+  @Override
+  public boolean onKeyDown(int keyCode, KeyEvent event) {
+    System.out.println("onKeyDown event: keyCode = " + event.getKeyCode());
+    return ! allowedKeys.contains(event.getKeyCode()); // prevent event from being propagated if not allowed
+  }
+
+  @Override
+  public void finish() {
+     super.finish();
+  }
+
+  // http://www.andreas-schrade.de/2015/02/16/android-tutorial-how-to-create-a-kiosk-mode-in-android/
+  @Override
+  public void onWindowFocusChanged(boolean hasFocus) {
+    super.onWindowFocusChanged(hasFocus);
+    if(!hasFocus) {
+      System.out.println("Focus lost - closing system dialogs");
+
+      Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+      sendBroadcast(closeDialog);
+
+      ActivityManager am = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+      am.moveTaskToFront(getTaskId(), ActivityManager.MOVE_TASK_WITH_HOME);
+
+      // sometime required to close opened notification area
+      Timer timer = new Timer();
+      timer.schedule(new TimerTask(){
+        public void run() {
+          Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+          sendBroadcast(closeDialog);
         }
-        
-        loadUrl(launchUrl);
-        
-        // https://github.com/apache/cordova-plugin-statusbar/blob/master/src/android/StatusBar.java
-        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        
-        // https://github.com/hkalina/cordova-plugin-kiosk/issues/14
-        View decorView = getWindow().getDecorView();
-        // Hide the status bar.
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
-        // Remember that you should never show the action bar if the
-        // status bar is hidden, so hide that too if necessary.
-        ActionBar actionBar = getActionBar();
-        if (actionBar != null) actionBar.hide();
-        
-        // add overlay to prevent statusbar access by swiping
-        statusBarOverlay = StatusBarOverlay.create(this);
+      }, 500); // 0.5 second
     }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (statusBarOverlay != null) {
-            statusBarOverlay.destroy(this);
-            statusBarOverlay = null;
-        }
-    }
-
-    @Override
-    protected void onPause() {
-            super.onPause();
-            ActivityManager activityManager = (ActivityManager) getApplicationContext()
-                    .getSystemService(Context.ACTIVITY_SERVICE);
-            activityManager.moveTaskToFront(getTaskId(), 0);
-    }     
-    
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        return ! allowedKeys.contains(event.getKeyCode()); // prevent event from being propagated if not allowed
-    }
-    
-    @Override
-    public void finish() {
-        // System.out.println("Never finish...");
-        super.finish();
-    }
-
-    // http://www.andreas-schrade.de/2015/02/16/android-tutorial-how-to-create-a-kiosk-mode-in-android/
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if(!hasFocus) {
-            System.out.println("Focus lost - closing system dialogs");
-            
-            Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
-            sendBroadcast(closeDialog);
-            
-            ActivityManager am = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
-            am.moveTaskToFront(getTaskId(), ActivityManager.MOVE_TASK_WITH_HOME);
-            
-            // sometime required to close opened notification area
-            Timer timer = new Timer();
-            timer.schedule(new TimerTask(){
-                public void run() {
-                    Intent closeDialog = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
-                    sendBroadcast(closeDialog);
-                }
-            }, 1); // 0.5 second
-        }
-    }
+  }
 }
-
--- android/StatusBarOverlay.java
+++ android/StatusBarOverlay.java
@@ -17,58 +17,72 @@ import java.lang.reflect.Method;
 // http://stackoverflow.com/questions/25284233/prevent-status-bar-for-appearing-android-modified
 public class StatusBarOverlay extends ViewGroup {
 
-    private static final int OVERLAY_PERMISSION_REQ_CODE = 4545;
+  private static final int OVERLAY_PERMISSION_REQ_CODE = 4545;
 
-    public StatusBarOverlay(Context context) {
-        super(context);
+  public StatusBarOverlay(Context context) {
+    super(context);
+  }
+
+  @Override
+  protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+  }
+
+  @Override
+  public boolean onInterceptTouchEvent(MotionEvent ev) {
+    return true;
+  }
+
+  static StatusBarOverlay create(Activity activity) {
+    WindowManager manager = ((WindowManager) activity
+      .getApplicationContext()
+      .getSystemService(Context.WINDOW_SERVICE));
+
+    WindowManager.LayoutParams localLayoutParams = new WindowManager.LayoutParams();
+    localLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
+    localLayoutParams.gravity = Gravity.TOP;
+    localLayoutParams.flags =
+      WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | // Enable the notification to receive touch events
+        WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | // Draw over status bar
+        WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+
+    localLayoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+    localLayoutParams.height = (int) (50 * activity.getResources().getDisplayMetrics().scaledDensity);
+    localLayoutParams.format = PixelFormat.TRANSPARENT;
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      localLayoutParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
     }
 
-    @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-    }
+    StatusBarOverlay view = new StatusBarOverlay(activity);
+    manager.addView(view, localLayoutParams);
 
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        return true;
-    }
+    System.out.println("Installing StatusBarOverlay");
+    return view;
+  }
 
-    static StatusBarOverlay create(Activity activity) {
-         WindowManager manager = ((WindowManager) activity.getApplicationContext()
-                .getSystemService(Context.WINDOW_SERVICE));
-
-        WindowManager.LayoutParams localLayoutParams = new WindowManager.LayoutParams();
-        localLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
-        localLayoutParams.gravity = Gravity.TOP;
-        localLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE|
-
-                // this is to enable the notification to recieve touch events
-                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
-
-                // Draws over status bar
-                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
-
-        localLayoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
-        int resId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
-        int result = 0;
-        if (resId > 0) {
-            result = activity.getResources().getDimensionPixelSize(resId);
+  static StatusBarOverlay createOrObtainPermission(Activity activity) {
+    if (Build.VERSION.SDK_INT >= 23) { // added in API level 23
+      try {
+        Method canDrawOverlays = Settings.class.getMethod("canDrawOverlays", Context.class);
+        if (! (Boolean)canDrawOverlays.invoke(null, activity)) {
+          Intent intent = new Intent("android.settings.action.MANAGE_OVERLAY_PERMISSION", //Settings.ACTION_MANAGE_OVERLAY_PERMISSION
+            Uri.parse("package:" + activity.getApplicationContext().getPackageName()));
+          activity.startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+          return create(activity);
         }
-
-        localLayoutParams.height = result;
-
-        localLayoutParams.format = PixelFormat.TRANSPARENT;
-        StatusBarOverlay view = new StatusBarOverlay(activity);
-
-        manager.addView(view, localLayoutParams);
-
-        return view;
+      } catch (Exception e) {
+        e.printStackTrace();
+        return null;
+      }
     }
-    
-    void destroy(Activity activity) {
-        WindowManager manager = ((WindowManager) activity
-                .getApplicationContext()
-                .getSystemService(Context.WINDOW_SERVICE));
-        manager.removeView(this);
-        System.out.println("Removing StatusBarOverlay");
-    }
+    return create(activity);
+  }
+
+  void destroy(Activity activity) {
+    WindowManager manager = ((WindowManager) activity
+      .getApplicationContext()
+      .getSystemService(Context.WINDOW_SERVICE));
+    manager.removeView(this);
+    System.out.println("Removing StatusBarOverlay");
+  }
 }
